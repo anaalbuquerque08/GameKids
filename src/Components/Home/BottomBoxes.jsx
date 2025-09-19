e2a906b0c08b1742146d720c225534ce2c70d3de
Ana Carolina Albuquerque <anacarolina.albuquerque.v@gmail.com>
2025-09-19 20:10:45 -0300

    Arrumei mais alguns bugs de responsividade

--- src/Components/Home/BottomBoxes.jsx
+++ src/Components/Home/BottomBoxes.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import mushroom from "../../assets/components/mushroom.png";
+
 
 export default function BottomBoxes() {
   return (
@@ -8,7 +10,7 @@ export default function BottomBoxes() {
       </div>
       <div className="geral-box box ">
         <div className="tab-line">
-      <div className="mushroom"></div>
+      <div className="mushroom"><img src={mushroom}alt="mushroom" /></div>
         </div>
   
       </div>
