cbbf9a6ac097ac81437444db1ec9b32255bc5948
Thiago Silva <titusilvs91@gmail.com>
2025-11-03 01:38:19 -0300

    feat: Adiciona jogo da memoria

--- src/Pages/GamePage.jsx
+++ src/Pages/GamePage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import JogoDaVelha from "../Games/Jogo-Da-Velha/src/App";
 import Quizz from "../Games/Quizz/src/App";
+import JogoDaMemoria from "../Games/Jogo-Da-Memoria/src/App"
 
 // ESSA PAGE AQUI ANAA BASICAMENTE VAI PEGAR O ID RENDERIZAR O COMPONENTE DO JOGO
 const GamePage = () => {
@@ -8,7 +9,8 @@ const GamePage = () => {
 
   const gameComponents = {
     "jogo-da-velha": <JogoDaVelha />,
-    "quizz": <Quizz />
+    "quizz": <Quizz />,
+    "memoria": <JogoDaMemoria />
   };
   return <div>{gameComponents[id] || <h2>Jogo não encontrado</h2>}</div>;
 };
